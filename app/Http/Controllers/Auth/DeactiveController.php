6fbd89d8ced4a3d9911a9d41798cdd91518afa6e
stst5821 <stst5821@gmail.com>
2021-02-04 08:33:51 +0900

    ユーザーの削除をハードデリートに変更。削除に合わせて投稿とコメントも削除されるようにした。

--- app/Http/Controllers/Auth/DeactiveController.php
+++ app/Http/Controllers/Auth/DeactiveController.php
@@ -18,7 +18,14 @@ class DeactiveController extends Controller
     
     public function Deactive()
     {
-        User::find(Auth::id())->delete();
+        // ↓ソフトデリート
+        // User::find(Auth::id())->delete();
+        
+        // ↓ハードデリート ユーザーに紐づく投稿とコメントも同時に削除する。
+        $user = User::find(Auth::id());
+        $user->comment()->delete();
+        $user->post()->delete();
+        $user->forceDelete();
         return redirect('/');
     }
 }
