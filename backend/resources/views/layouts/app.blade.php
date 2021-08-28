37fe15df6a8aeffdacdd606629c7e5f531ebb956
stst5821 <stst5821@gmail.com>
2021-08-28 10:26:14 +0900

    app.jsの読み込み用コードが2つあったのでhead側を削除

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -11,9 +11,6 @@
     <!-- .envに設定したAPP_NAMEを表示させる -->
     <title>{{ config('app.name') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
