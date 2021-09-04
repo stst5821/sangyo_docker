d12cea2fd097ea2bf40f54d80527c3df7ffbfde7
stst5821 <stst5821@gmail.com>
2021-09-04 20:22:02 +0900

    footerを下に固定

--- backend/resources/views/components/app.blade.php
+++ backend/resources/views/components/app.blade.php
@@ -23,14 +23,12 @@
 </head>
 
 <body>
-    <!-- ヘッダーコンポーネント -->
     <x-header />
-    <!-- コンテンツの中身を表示 -->
     <main class="py-4">
         {{$slot}}
     </main>
     <x-footer />
-    
+
     <!-- トランスパイルしたJSをBladeに読み込ませる -->
     <script src="{{ mix('/js/app.js') }}"></script>
     <!-- navのプルダウンを有効にするため、以下のjqueryとbootstrapのCDNを読み込んでいる -->
